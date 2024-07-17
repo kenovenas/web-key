cc38b2460ceba386dfee9f634f9428424a5c948d
kenovenas <136008924+kenovenas@users.noreply.github.com>
2024-07-16 22:39:35 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,27 +1,26 @@
-# app.py
-
 from flask import Flask, request, jsonify
 import random
 import string
 
 app = Flask(__name__)
 
+# Armazenar as chaves geradas
+generated_keys = {}
+
 @app.route('/generate-key', methods=['GET'])
 def generate_key():
     key = ''.join(random.choices(string.ascii_letters + string.digits, k=10))
-    return jsonify({'key': key})
+    generated_keys[key] = True  # Armazena a chave como válida
+    return jsonify(key=key)
 
 @app.route('/validate-key', methods=['POST'])
 def validate_key():
     data = request.get_json()
-    key_to_validate = data.get('key')
-
-    # Exemplo de validação simples
-    valid_keys = ['abc123', 'def456']  # Lista de chaves válidas
-    if key_to_validate in valid_keys:
-        return jsonify({'valid': True})
+    key = data.get('key')
+    if key in generated_keys:
+        return jsonify(valid=True)
     else:
-        return jsonify({'valid': False})
+        return jsonify(valid=False)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
